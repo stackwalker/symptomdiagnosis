99cee4ab5620bf13726d6ba461400be89d67cae9
Nate Logan <nlogan@healthwise.org>
2017-11-10 12:01:57 -0700

    Incorporating React form component

--- symptomdiagnosisui/ClientApp/components/Home.tsx
+++ symptomdiagnosisui/ClientApp/components/Home.tsx
@@ -1,28 +1,19 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
+import FormControls from './FormControls'
+
 export class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return (
             <div className="row">
                 <div className='col-sm-2'>
-                    <div className='configuration-sidebar'>
-                        <div className='config-box'>
-                            <div>Load Dataset</div>
-                            <select>
-                                <option>10 Encounters</option>
-                                <option>10000 Encounters</option>
-                            </select>
-                        </div>
-                        <div className='config-box'>
-                            <div>Add a Symptom:</div>
-                            <input type="text"/>
-                            <button>Add</button>
-                        </div>
-                        <div className='config-box'>
 
-                        </div>
-                    </div>
+                    <FormControls
+                        onChangeNumberOfResults={function(num) { }}
+                        onChangeSymptoms={function(sxArr) { }}
+                    />
+
                 </div>
                 <div className='col-sm-6'>
                     <h3>Results</h3>
