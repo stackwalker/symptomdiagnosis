5fc262e0ce17625eab4669667643eb50ee2c515a
Nate Logan <nlogan@healthwise.org>
2017-11-15 09:19:33 -0700

    Adding nice visualization and tweaking UI

--- symptomdiagnosis/client/Form.jsx
+++ symptomdiagnosis/client/Form.jsx
@@ -50,7 +50,7 @@ class Form extends React.Component {
       <ul>
         {this.state.symptoms.map((symptom, i) => {
           return (
-            <li key={"symtom_" + i}>
+            <li className="symptom" key={"symtom_" + i}>
               {symptom.name}
               <svg viewBox="0 0 40 40" onClick={function(){this.onDeleteSymptom(symptom.name)}.bind(this)}>
                 <path className="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" />
--- symptomdiagnosis/client/ResultList.jsx
+++ symptomdiagnosis/client/ResultList.jsx
@@ -2,11 +2,28 @@ import React from 'react'
 
 class ResultList extends React.Component {
   render() {
+    // scale the values to determine opacity
+    const scale = function (num, lowScale, hiScale, min, max) {
+      return (hiScale - lowScale) * (num - min) / (max - min) + lowScale
+    }
+
     return (
       <div>
         {
           this.props.results && this.props.results.map(dx => {
             return <li key={dx.name}>{dx.name}</li>
+          })
+        }
+      </div>
+
+      <div>
+        {
+          this.props.results && this.props.results.map(dx => {
+            const opacity = scale(dx.Count, 0.25, 1, this.props.results[this.props.results.length-1].Count, this.props.results[0].Count)
+            // JavaScript laughing in the face of its strongly typed relatives:
+            const fontSize = opacity * 48 + 'px'
+
+            return <li style={{ opacity: opacity, fontSize: fontSize}} key={dx.name}>{dx.name}</li>
           })
         }
       </div>
